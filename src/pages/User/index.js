1cb13df6786e9d270234b5d1e93bd6deb61ae5d4
Daniel Moreira <dmcardoso.99@hotmail.com>
2020-04-07 23:19:36 -0300

    :rocket: Adiciona loader

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
+import { ActivityIndicator } from 'react-native';
+
 import {
     Container,
     Header,
@@ -18,17 +20,33 @@ import {
 
 export default function User({ navigation }) {
     const [stars, setStars] = useState([]);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
     const user = navigation.getParam('user');
 
+    async function getStars(pageRequest = 1) {
+        setLoading(true);
+        const response = await api.get(`/users/${user.login}/starred`, {
+            params: {
+                per_page: 5,
+                page: pageRequest,
+            },
+        });
+
+        setStars(page >= 2 ? [...stars, ...response.data] : response.data);
+        setLoading(false);
+        setPage(pageRequest);
+    }
+
     useEffect(() => {
-        async function getStars() {
-            const response = await api.get(`/users/${user.login}/starred`);
-
-            setStars(response.data);
-        }
-
         getStars();
     }, []);
+
+    async function loadMore() {
+        const nextPage = page + 1;
+
+        getStars(nextPage);
+    }
 
     return (
         <Container>
@@ -37,19 +55,26 @@ export default function User({ navigation }) {
                 <Name>{user.name}</Name>
                 <Bio>{user.bio}</Bio>
             </Header>
-            <Stars
-                data={stars}
-                keyExtractor={star => String(star.id)}
-                renderItem={({ item }) => (
-                    <Starred>
-                        <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-                        <Info>
-                            <Title>{item.name}</Title>
-                            <Author>{item.owner.login}</Author>
-                        </Info>
-                    </Starred>
-                )}
-            />
+            {stars.length > 0 && (
+                <Stars
+                    onEndReachedThreshold={0.2}
+                    onEndReached={loadMore}
+                    data={stars}
+                    keyExtractor={star => String(star.id)}
+                    renderItem={({ item }) => (
+                        <Starred>
+                            <OwnerAvatar
+                                source={{ uri: item.owner.avatar_url }}
+                            />
+                            <Info>
+                                <Title>{item.name}</Title>
+                                <Author>{item.owner.login}</Author>
+                            </Info>
+                        </Starred>
+                    )}
+                />
+            )}
+            {loading && <ActivityIndicator color="#7159c1" />}
         </Container>
     );
 }
